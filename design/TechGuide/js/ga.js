03b855957371e8844d3630d00ec33bddc947f047
Yuta Takahashi <m1205123ing@gmail.com>
2022-08-16 22:44:04 +0900

    bugfix

--- design/TechGuide/js/ga.js
+++ design/TechGuide/js/ga.js
@@ -1,8 +1,18 @@
-<!-- Google tag (gtag.js) -->
-<script async src=“https://www.googletagmanager.com/gtag/js?id=G-MS3VSF32XF”></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag(‘js’, new Date());
-  gtag(‘config’, ‘G-MS3VSF32XF’);
-</script>
+function loadScript(src) {
+  return new Promise(function (resolve, reject) {
+    var s;
+    s = document.createElement('script');
+    s.src = src;
+    s.onload = resolve;
+    s.onerror = reject;
+    document.head.appendChild(s);
+  });
+}
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+var src = "https://www.googletagmanager.com/gtag/js?id=G-MS3VSF32XF";
+loadScript(src).then(function () {
+  gtag('js', new Date());
+  gtag('config', 'G-MS3VSF32XF');
+});
