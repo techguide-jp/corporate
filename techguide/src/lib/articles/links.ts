b45e34dac8d0b6a2403b0cc4721d5ca050fe1dcd
Yuta Takahashi <m1205123ing@gmail.com>
2026-07-08 07:17:25 +0900

    refactor: 記事データを記事ごとに分割

---
+++ techguide/src/lib/articles/links.ts
@@ -0,0 +1,4 @@
+export const readyMockReferralParams =
+  'utm_source=techguide&utm_medium=owned_article&utm_campaign=ready_mock_referral';
+
+export const readyMockHomeHref = `https://www.ready-mock.com/?${readyMockReferralParams}&utm_content=ready_mock_text_link`;
