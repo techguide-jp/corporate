d4cfb24b2acfaa3a0c1b09432137d68a82b4dee5
Yuta Takahashi <m1205123ing@gmail.com>
2026-07-13 10:33:09 +0900

    feat: 事業と技術の認識合わせの記事を追加

--- techguide/src/lib/articles/articles.ts
+++ techguide/src/lib/articles/articles.ts
@@ -1,4 +1,5 @@
 import { beforeToolSelectionArticle } from './data/before-tool-selection';
+import { businessEngineerAlignmentArticle } from './data/business-engineer-alignment';
 import { designingForMaintenanceAndImprovementArticle } from './data/designing-for-maintenance-and-improvement';
 import { whyWeStartWithPlanningArticle } from './data/why-we-start-with-planning';
 import type { Article } from './types';
@@ -7,4 +8,5 @@ export const articles = [
   whyWeStartWithPlanningArticle,
   beforeToolSelectionArticle,
   designingForMaintenanceAndImprovementArticle,
+  businessEngineerAlignmentArticle,
 ] satisfies readonly Article[];
